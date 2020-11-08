05b0df2f6a3f13da8c271dc7ee44f6a6d473aa10
JonathanYangCMSC203 <60461439+JonathanYangCMSC203@users.noreply.github.com>
2020-11-07 22:51:38 -0500

    Update testDriver.java

--- testDriver.java
+++ testDriver.java
@@ -4,22 +4,20 @@ public class testDriver {
 	//Test area: remember to delete
 	public static void main(String args[])
 	{
-		CheckingAccount a = new CheckingAccount("Jon", 1000);
-		System.out.println("a " + a.withdraw(500));
-		SavingsAccount b = new SavingsAccount("Jo", 2000);
-		System.out.println("b " + b.getAccountNumber());
-		SavingsAccount c = new SavingsAccount("Jo", 2000);
-		System.out.println("c " + c.getAccountNumber());
-		SavingsAccount d = new SavingsAccount(c, 2000);
-		System.out.println("d " + d.getAccountNumber());
+		SavingsAccount a = new SavingsAccount("Jo", 2000);
+		System.out.println("a: " + a.getAccountNumber());//Should return 100001-0
+		SavingsAccount b = new SavingsAccount (a, 2000);
+		System.out.println("b: " + b.getAccountNumber());//Should return 100001-1
+		SavingsAccount c = new SavingsAccount (b, 2000);
+		System.out.println("c: " + c.getAccountNumber());//Should return 100001-2
 		
-		SavingsAccount e = new SavingsAccount("jo", 2000);
-		System.out.println("e " + e.getAccountNumber());
+		System.out.println();
+		
+		SavingsAccount d = new SavingsAccount("Jo", 2000);
+		System.out.println("d: " + d.getAccountNumber());//Should return 100002-0
+		SavingsAccount e = new SavingsAccount(d, 2000);
+		System.out.println("e: " + e.getAccountNumber());//Should return 100002-1
 		SavingsAccount f = new SavingsAccount(e, 2000);
-		System.out.println("f " + f.getAccountNumber());
-		SavingsAccount g = new SavingsAccount(f, 2000);
-		System.out.println("g " + g.getAccountNumber());
-		SavingsAccount h = new SavingsAccount(d, 2000);
-		System.out.println("h " + h.getAccountNumber());
+		System.out.println("f: " + f.getAccountNumber());//Should return 100002-2
 	}
 }
